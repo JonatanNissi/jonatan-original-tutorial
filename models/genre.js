02b9ee67c489291471b86b2a492d3c0581d99e32
JonatanNissi <jonu.nissi@gmail.com>
2019-04-24 20:08:40 +0300

    Original tutorial

---
+++ models/genre.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema(
+  {
+    name: {type: String, required: true, min: 3, max: 100},
+  }
+);
+
+// Virtual for genre
+GenreSchema
+.virtual('genre')
+.get(function () {
+  return this.name + ', ' + this.name;
+});
+
+// Virtual for genre's URL
+GenreSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/genre/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Genre', GenreSchema);
